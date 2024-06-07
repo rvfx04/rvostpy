cc0008b4f10d844cbaf2d3c6eeaa993ae74f174a
rvfx04 <rvfx04@gmail.com>
2024-06-07 15:39:02 -0500

    Update appgtop.py

--- appgtop.py
+++ appgtop.py
@@ -8,6 +8,9 @@ def init_connection():
     return pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER="
         + st.secrets["server"]+ ";DATABASE="+ st.secrets["database"]+ ";UID="
         + st.secrets["username"]+ ";PWD="+ st.secrets["password"])
+    
+# Título de la aplicación
+st.title("Consulta de Orden de Producción")
 
 conn = init_connection()
 
