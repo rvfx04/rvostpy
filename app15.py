f2f66c2d128c1cb167ca4f995d616140b00ab410
rvfx04 <rvfx04@gmail.com>
2024-06-07 12:26:34 -0500

    Update app15.py

--- app15.py
+++ app15.py
@@ -30,17 +30,17 @@ df = pd.DataFrame(rows, columns=columns)
 df = df.set_index(df.columns[0])
 
 # Obtener valores únicos del campo 'idgrupodefecto'
-#unique_idgrupodefecto = df['idgrupodefecto'].unique()
+unique_idgrupodefecto = df['idgrupodefecto'].unique()
 
 # Crear un multiselect para escoger los valores del campo 'idgrupodefecto'
-#selected_idgrupodefecto = st.multiselect('Seleccione idgrupodefecto:', unique_idgrupodefecto)
+selected_idgrupodefecto = st.multiselect('Seleccione idgrupodefecto:', unique_idgrupodefecto)
 
 # Filtrar el DataFrame según los valores seleccionados
-#if selected_idgrupodefecto:
-#    filtered_df = df[df['idgrupodefecto'].isin(selected_idgrupodefecto)]
-#else:
-#   filtered_df = df
+if selected_idgrupodefecto:
+    filtered_df = df[df['idgrupodefecto'].isin(selected_idgrupodefecto)]
+else:
+   filtered_df = df
 
 # Mostrar el DataFrame filtrado
-#st.dataframe(filtered_df)
-st.dataframe(df) 
+st.dataframe(filtered_df)
+#st.dataframe(df) 
