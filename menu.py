669ce6c5ea24136f5afeceaf85de03d768c94912
rvfx04 <rvfx04@gmail.com>
2024-06-07 18:44:06 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -4,20 +4,20 @@ def main():
     st.title("Menú de Navegación")
 
     # Crear el menú de navegación
-    menu = ["Home", "App 1: prurvo", "App 2: rvo012"]
+    menu = ["Home", "App 1: appgtpedido", "App 2: appgtop"]
     choice = st.sidebar.selectbox("Selecciona una aplicación", menu)
 
     if choice == "Home":
         st.subheader("Bienvenido al Menú de Navegación")
         st.write("Selecciona una aplicación desde la barra lateral.")
-    elif choice == "App 1: prurvo":
-        st.subheader("App 1: prurvo")
+    elif choice == "App 1: appgtpedido":
+        st.subheader("App 1: appgtpedido")
         st.write("Haga clic en el siguiente enlace para ir a la aplicación:")
-        st.markdown("[Ir a prurvo](https://prurvo.streamlit.app)")
-    elif choice == "App 2: rvo012":
-        st.subheader("App 2: rvo012")
+        st.markdown("[Ir a appgtpedido](https://appgtpedido.streamlit.app)")
+    elif choice == "App 2: appgtop":
+        st.subheader("App 2: appgtop")
         st.write("Haga clic en el siguiente enlace para ir a la aplicación:")
-        st.markdown("[Ir a rvo012](https://rvo012.streamlit.app)")
+        st.markdown("[Ir a appgtop](https://appgtop.streamlit.app)")
 
 if __name__ == '__main__':
     main()
