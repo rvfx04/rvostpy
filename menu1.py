469e13bfa14239e6f129f672719bc24ee3cc6bc2
rvfx04 <rvfx04@gmail.com>
2024-06-08 21:44:58 -0500

    Update menu1.py

--- menu1.py
+++ menu1.py
@@ -1,14 +1,8 @@
 import streamlit as st
 
 def main():
-    st.title("Menú 11")
-
-    # Botón para acceder a la aplicación prurvo
-    #if st.button("Ir a App 1: prurvo"):
+    st.title("Confección")
     st.markdown("[Ir a Pedido](https://appgtpedido.streamlit.app)")
-    
-    # Botón para acceder a la aplicación rvo012
-    #if st.button("Ir a App 2: rvo012"):
     st.markdown("[Ir a OP](https://appgtop.streamlit.app)")
 
 if __name__ == '__main__':
