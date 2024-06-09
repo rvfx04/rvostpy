544351ae62c18a2ae27336cf8c31669d7cef7fb6
rvfx04 <rvfx04@gmail.com>
2024-06-08 21:36:16 -0500

    Update menu1.py

--- menu1.py
+++ menu1.py
@@ -1,28 +1,15 @@
 import streamlit as st
 
 def main():
-    st.title("Menú")
-
-    # Instrucciones para usar el menú
-    st.write("Haga clic en uno de los siguientes botones para ir a la aplicación correspondiente:")
+    st.title("Menú de Navegación")
 
     # Botón para acceder a la aplicación prurvo
     if st.button("Ir a App 1: prurvo"):
-        js_code = """
-        <script>
-        window.open('https://prurvo.streamlit.app', '_blank').focus();
-        </script>
-        """
-        st.markdown(js_code, unsafe_allow_html=True)
+        st.markdown('<meta http-equiv="refresh" content="0; url=https://prurvo.streamlit.app" />', unsafe_allow_html=True)
     
     # Botón para acceder a la aplicación rvo012
     if st.button("Ir a App 2: rvo012"):
-        js_code = """
-        <script>
-        window.open('https://rvo012.streamlit.app', '_blank').focus();
-        </script>
-        """
-        st.markdown(js_code, unsafe_allow_html=True)
+        st.markdown('<meta http-equiv="refresh" content="0; url=https://rvo012.streamlit.app" />', unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
