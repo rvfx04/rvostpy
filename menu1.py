f3eb3477d125e4be79ca5be6624bbfb4928c94a6
rvfx04 <rvfx04@gmail.com>
2024-06-08 21:28:59 -0500

    Update menu1.py

--- menu1.py
+++ menu1.py
@@ -1,34 +1,21 @@
 import streamlit as st
+import webbrowser
 
-def main():
-    st.title("Menú de Navegación")
+# Crear las opciones del menú
+menu_options = ["Home", "Google", "YouTube"]
 
-    # Crear el menú de navegación
-    menu = ["Home", "Pedido", "OP"]
-    choice = st.sidebar.selectbox("Selecciona una aplicación", menu)
+# Seleccionar una opción del menú
+choice = st.sidebar.selectbox("Selecciona una opción", menu_options)
 
-    if choice == "Home":
-        st.subheader("Bienvenido al Menú de Navegación")
-        st.write("Selecciona una aplicación desde la barra lateral.")
-    elif choice == "Pedido":
-        st.experimental_set_query_params(app="pedido")
-        st.write("Redirigiendo a Pedido...")
-        st.experimental_rerun()
-    elif choice == "OP":
-        st.experimental_set_query_params(app="op")
-        st.write("Redirigiendo a OP...")
-        st.experimental_rerun()
+# Diccionario de opciones y URLs correspondientes
+url_dict = {
+    "Google": "https://www.google.com",
+    "YouTube": "https://www.youtube.com"
+}
 
-if __name__ == '__main__':
-    main()
-
-# Manejar la redirección según los parámetros de consulta
-query_params = st.experimental_get_query_params()
-if "app" in query_params:
-    app = query_params["app"][0]
-    if app == "pedido":
-        st.write("Redirigiendo a Pedido...")
-        st.experimental_rerun("https://appgtpedido.streamlit.app")
-    elif app == "op":
-        st.write("Redirigiendo a OP...")
-        st.experimental_rerun("https://appgtop.streamlit.app")
+# Redirigir al usuario a la URL seleccionada
+if choice in url_dict:
+    st.write(f"Redirigiendo a {choice}...")
+    webbrowser.open_new_tab(url_dict[choice])
+else:
+    st.write("Selecciona una opción del menú para abrir una URL.")
