ae58e67e1733faa38d4ad5c4264d66ca19631c37
rvfx04 <rvfx04@gmail.com>
2024-06-08 21:45:26 -0500

    Update menu1.py

--- menu1.py
+++ menu1.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 def main():
     st.title("Confección")
-    st.markdown("[Ir a Pedido](https://appgtpedido.streamlit.app)")
-    st.markdown("[Ir a OP](https://appgtop.streamlit.app)")
+    st.markdown("[Pedido](https://appgtpedido.streamlit.app)")
+    st.markdown("[OP](https://appgtop.streamlit.app)")
 
 if __name__ == '__main__':
     main()
