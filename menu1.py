26fffa3ac5860f7904a37576eb9de325c72d3583
rvfx04 <rvfx04@gmail.com>
2024-06-08 21:43:01 -0500

    Update menu1.py

--- menu1.py
+++ menu1.py
@@ -5,11 +5,11 @@ def main():
 
     # Botón para acceder a la aplicación prurvo
     #if st.button("Ir a App 1: prurvo"):
-        st.markdown("[Ir a Pedido](https://appgtpedido.streamlit.app)")
+    st.markdown("[Ir a Pedido](https://appgtpedido.streamlit.app)")
     
     # Botón para acceder a la aplicación rvo012
     #if st.button("Ir a App 2: rvo012"):
-        st.markdown("[Ir a OP](https://appgtop.streamlit.app)")
+    st.markdown("[Ir a OP](https://appgtop.streamlit.app)")
 
 if __name__ == '__main__':
     main()
