47fe51a564b8c0187dc3cd2999fbfe66d3ede94b
rvfx04 <rvfx04@gmail.com>
2024-06-08 21:31:34 -0500

    Update menu1.py

--- menu1.py
+++ menu1.py
@@ -1,21 +1,28 @@
 import streamlit as st
-import webbrowser
 
-# Crear las opciones del menú
-menu_options = ["Home", "Google", "YouTube"]
+def main():
+    st.title("Menú de Navegación")
 
-# Seleccionar una opción del menú
-choice = st.sidebar.selectbox("Selecciona una opción", menu_options)
+    # Instrucciones para usar el menú
+    st.write("Haga clic en uno de los siguientes botones para ir a la aplicación correspondiente:")
 
-# Diccionario de opciones y URLs correspondientes
-url_dict = {
-    "Google": "https://www.google.com",
-    "YouTube": "https://www.youtube.com"
-}
+    # Botón para acceder a la aplicación prurvo
+    if st.button("Ir a App 1: prurvo"):
+        js_code = """
+        <script>
+        window.open('https://prurvo.streamlit.app', '_blank').focus();
+        </script>
+        """
+        st.markdown(js_code, unsafe_allow_html=True)
+    
+    # Botón para acceder a la aplicación rvo012
+    if st.button("Ir a App 2: rvo012"):
+        js_code = """
+        <script>
+        window.open('https://rvo012.streamlit.app', '_blank').focus();
+        </script>
+        """
+        st.markdown(js_code, unsafe_allow_html=True)
 
-# Redirigir al usuario a la URL seleccionada
-if choice in url_dict:
-    st.write(f"Redirigiendo a {choice}...")
-    webbrowser.open_new_tab(url_dict[choice])
-else:
-    st.write("Selecciona una opción del menú para abrir una URL.")
+if __name__ == '__main__':
+    main()
