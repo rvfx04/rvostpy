8248cad73b19798e9401d22b9c8a96cd20a85983
rvfx04 <rvfx04@gmail.com>
2024-06-13 15:45:22 -0500

    Update telasqc.py

--- telasqc.py
+++ telasqc.py
@@ -62,7 +62,7 @@ def get_data(start_date, end_date, clientes, codigo, tela, color, acabado, parti
     return df
 
 # Interfaz de usuario
-st.title('Consulta de Reportes de Calidad')
+st.title('Análisis de los Reportes de Calidad')
 
 # Organización en dos columnas
 col1, col2 = st.columns(2)
