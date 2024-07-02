8b7de20a8f4da28b3abd547365a9d80c3a426c1c
rvfx04 <rvfx04@gmail.com>
2024-07-02 10:53:16 -0500

    Update pru.py

--- pru.py
+++ pru.py
@@ -61,7 +61,10 @@ query = f"""
             a.IdtdDocumentoForm = 10
             AND a.IdtdTipoVenta = 4
     """
+# Mostrar el número de registros
+st.write(f"Número de registros: {len(df)}")
     
+
 # Ejecutar la consulta y mostrar los resultados
 df = execute_query(query)
 st.dataframe(df)
