bb06a734e3c444c212663bf40a96254dab10dff3
rvfx04 <rvfx04@gmail.com>
2024-07-02 08:15:05 -0500

    Update pru.py

--- pru.py
+++ pru.py
@@ -62,10 +62,10 @@ def load_data(start_date, end_date, pedido, cliente, po):
         WHERE
             a.IdtdDocumentoForm = 10
             AND a.IdtdTipoVenta = 4
-            AND (CASE WHEN ISDATE(a.dtFechaEntrega) = 1 THEN CONVERT(DATE, a.dtFechaEntrega) ELSE NULL END) BETWEEN '{start_date}' AND '{end_date}'
-            AND a.CoddocOrdenVenta LIKE '%{pedido}%'
-            AND b.NommaeAnexoCliente LIKE '%{cliente}%'
-            AND a.nvDocumentoReferencia LIKE '%{po}%'
+            #AND (CASE WHEN ISDATE(a.dtFechaEntrega) = 1 THEN CONVERT(DATE, a.dtFechaEntrega) ELSE NULL END) BETWEEN '{start_date}' AND '{end_date}'
+            #AND a.CoddocOrdenVenta LIKE '%{pedido}%'
+            #AND b.NommaeAnexoCliente LIKE '%{cliente}%'
+            #AND a.nvDocumentoReferencia LIKE '%{po}%'
         """
         conn = get_connection()
         if conn:
