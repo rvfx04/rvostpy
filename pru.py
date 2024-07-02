eb2a770b576045a9c43fbb229a4f29ef0399a119
rvfx04 <rvfx04@gmail.com>
2024-07-02 08:01:15 -0500

    Create pru.py

---
+++ pru.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import pyodbc
+from datetime import datetime, timedelta
+
+# Función para conectar a la base de datos
+def get_connection():
+    try:
+        conn = pyodbc.connect(
+            "DRIVER={ODBC Driver 17 for SQL Server};"
+            "SERVER=" + st.secrets["server"] + ";"
+            "DATABASE=" + st.secrets["database"] + ";"
+            "UID=" + st.secrets["username"] + ";"
+            "PWD=" + st.secrets["password"] + ";"
+        )
+        return conn
+    except Exception as e:
+        st.error(f"Error al conectar a la base de datos: {e}")
+        return None
