d0b693e41ff59d12142d47c2b09c74f952253945
rvfx04 <rvfx04@gmail.com>
2024-07-03 18:29:16 -0500

    Update avancepedidos.py

--- avancepedidos.py
+++ avancepedidos.py
@@ -101,7 +101,7 @@ with st.sidebar:
         filtered_df = pd.concat([filtered_df, totals_df], ignore_index=True)
         with columns[0]:
             st.write(f"Número de registros: {len(filtered_df)-1}")
-            st.dataframe(filtered_df, hide_index=True)
+            st.dataframe(filtered_df[['PEDIDO'],['CLIENTE']], hide_index=True)
     else:
         with columns[0]:
             st.write("Por favor, aplica los filtros para ver resultados.")
