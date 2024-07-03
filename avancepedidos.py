600c925bfc84816cb067d54810b6401c052befd6
rvfx04 <rvfx04@gmail.com>
2024-07-03 18:45:07 -0500

    Update avancepedidos.py

--- avancepedidos.py
+++ avancepedidos.py
@@ -99,11 +99,11 @@ with st.sidebar:
         totals = filtered_df.select_dtypes(include=["int", "float"]).sum().rename("Total")
         totals_df = pd.DataFrame(totals).T
         filtered_df = pd.concat([filtered_df, totals_df], ignore_index=True)
-	    
 	with columns[0]:
-		st.write(f"Número de registros: {len(filtered_df)-1}")
-		filtered_df= filtered_df[['PEDIDO'],['CLIENTE']]
-		st.dataframe(filtered_df, hide_index=True)
+            st.write(f"Número de registros: {len(filtered_df)-1}")
+			filtered_df= filtered_df[['PEDIDO'],['CLIENTE']]
+			st.dataframe(filtered_df, hide_index=True)
+	 
     else:
         with columns[0]:
             st.write("Por favor, aplica los filtros para ver resultados.")
