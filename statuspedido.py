e4206a13b03a6fa0db3ff786122a63df2fe794c6
rvfx04 <rvfx04@gmail.com>
2024-07-04 16:02:33 -0500

    Update statuspedido.py

--- statuspedido.py
+++ statuspedido.py
@@ -120,8 +120,8 @@ if st.sidebar.button("Aplicar filtros"):
         #columns_to_show = ['columna1', 'columna2', 'columna3']
         #st.dataframe(data[columns_to_show])
         st.write(f"Por armar {len(kgxarm_df)-1} registros")
-        st.dataframe(kgxarm_df('PEDIDO','F_EMISION'), hide_index=True)
-        #st.dataframe(kgxarm_df(['PEDIDO','F_EMISION']), hide_index=True)
+        #st.dataframe(kgxarm_df('PEDIDO','F_EMISION'), hide_index=True)
+        st.dataframe(kgxarm_df(['PEDIDO','F_EMISION']), hide_index=True)
         
         kgxtenir_df = data.loc[data['KG_ARM_X_TEÑIR'] > 0]
         totals = kgxtenir_df.select_dtypes(include=["int", "float"]).sum().rename("Total")
