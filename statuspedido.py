b6a239de4f70a9ab4292d968e730d6fb1cf9bab7
rvfx04 <rvfx04@gmail.com>
2024-07-04 15:40:54 -0500

    Update statuspedido.py

--- statuspedido.py
+++ statuspedido.py
@@ -76,17 +76,7 @@ def load_data(start_date, end_date, pedido, cliente, po):
         conn = get_connection()
         if conn:
             df = pd.read_sql(query, conn)
-            #df = df.set_index(df.columns[0]) # Quita la columna que numera los registros
-            #st.write(f"Número de registros: {len(df)}") # Muestra el número de registros de la tabla
             
-            # Calcular totales
-            #totals = df.sum(numeric_only=True)
-            #totals['CLIENTE'] = 'TOTAL' # La etiqueta de TOTAL la coloca al final de la columna CLIENTE
-            #totals_df = pd.DataFrame(totals).transpose()
-            #df = pd.concat([df, totals_df], ignore_index=True)
-            
-            #df = df.set_index(df.columns[0]) # Quita la columna que numera los registros
-          
             conn.close()
             return df
         else:
