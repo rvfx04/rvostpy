24bcd387becb13ab24543dcd52106f7668ccfee5
rvfx04 <rvfx04@gmail.com>
2024-07-04 08:43:40 -0500

    Update stapedido.py

--- stapedido.py
+++ stapedido.py
@@ -107,12 +107,12 @@ with st.sidebar:
            
             
            
-            #kgxarm_df = filtered_df.loc[df['KG_X_ARM'] > 0]
-            #totals = kgxarm_df.select_dtypes(include=["int", "float"]).sum().rename("Total")
-            #totals_df = pd.DataFrame(totals).T
-            #kgxarm_df = pd.concat([kgxarm_df, totals_df], ignore_index=True)
-            #st.write(f"Por Armar {len(kgxarm_df)-1} registros")
-            #st.dataframe(kgxarm_df, hide_index=True)
+            kgxarm_df = filtered_df.loc[df['KG_X_ARM'] > 0]
+            totals = kgxarm_df.select_dtypes(include=["int", "float"]).sum().rename("Total")
+            totals_df = pd.DataFrame(totals).T
+            kgxarm_df = pd.concat([kgxarm_df, totals_df], ignore_index=True)
+            st.write(f"Por Armar {len(kgxarm_df)-1} registros")
+            st.dataframe(kgxarm_df, hide_index=True)
 
             kgxtenir_df = filtered_df.loc[df['KG_ARM_X_TEÑIR'] > 0]
             totals = kgxtenir_df.select_dtypes(include=["int", "float"]).sum().rename("Total")
