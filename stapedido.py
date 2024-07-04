adba2f6873987f10829a81a013335e7194093660
rvfx04 <rvfx04@gmail.com>
2024-07-03 23:52:26 -0500

    Update stapedido.py

--- stapedido.py
+++ stapedido.py
@@ -102,9 +102,9 @@ with st.sidebar:
         with columns[0]:
             st.write(f"Número de registros: {len(filtered_df)-1}")
             #filtered_df = df.loc[:, ['PEDIDO', 'CLIENTE']]
-            kgxproduc_df = filtered_df.loc[df['KG_X_PRODUC'] > 0
-            kgxarm_df = filtered_df.loc[df['KG_X_ARM'] > 0
-            kgxtenir_df = filtered_df.loc[df['KG_X_TEÑIR'] > 0
+            kgxproduc_df = filtered_df.loc[df['KG_X_PRODUC'] > 0]
+            kgxarm_df = filtered_df.loc[df['KG_X_ARM'] > 0]
+            kgxtenir_df = filtered_df.loc[df['KG_X_TEÑIR'] > 0]
             
             st.dataframe(filtered_df, hide_index=True)
             
