f7b8df35ecde123167cd79c1976e1f9025c6a436
rvfx04 <rvfx04@gmail.com>
2024-07-15 09:30:52 -0500

    Update gtpedido.py

--- gtpedido.py
+++ gtpedido.py
@@ -213,7 +213,7 @@ cliente = st.sidebar.text_input("Cliente")
 po = st.sidebar.text_input("PO")
 
 # Botón para aplicar filtros
-if st.sidebar.button("Aplicar filtro"):
+if st.sidebar.button("Aplicar filtros"):
     
     data = load_data(start_date, end_date, pedido, cliente, po)
     
