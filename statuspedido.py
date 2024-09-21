e399779a617003a20339bea0a6f6ac21e00d3e4f
rvfx04 <rvfx04@gmail.com>
2024-09-21 13:22:20 -0500

    Update statuspedido.py

--- statuspedido.py
+++ statuspedido.py
@@ -47,8 +47,8 @@ def load_data(start_date, end_date, pedido, cliente, po):
         INNER JOIN dbo.maeCentroCosto d WITH (NOLOCK)
             ON b.IdmaeCentroCosto = d.IdmaeCentroCosto
             AND d.bConOrdenProduccion = 1
-        WHERE c.bCerrado = 0
-            AND c.bAnulado = 0
+        WHERE --c.bCerrado = 0 AND
+            c.bAnulado = 0
             AND c.IdtdDocumentoForm = 127
             AND b.IdmaeCentroCosto = 29
         GROUP BY g.IdDocumento_OrdenVenta
@@ -67,7 +67,7 @@ def load_data(start_date, end_date, pedido, cliente, po):
             AND b.dCantidadIng <> 0
         INNER JOIN dbo.docOrdenProduccion c WITH (NOLOCK)
             ON a.IdDocumento_OrdenProduccion = c.IdDocumento_OrdenProduccion
-            AND c.bCerrado = 0
+            --AND c.bCerrado = 0
             AND c.bAnulado = 0
             AND c.IdtdDocumentoForm = 127
         INNER JOIN dbo.docOrdenVenta g WITH (NOLOCK)
@@ -102,7 +102,7 @@ def load_data(start_date, end_date, pedido, cliente, po):
             AND b.dCantidadIng <> 0
         INNER JOIN dbo.docOrdenProduccion c WITH (NOLOCK)
             ON a.IdDocumento_OrdenProduccion = c.IdDocumento_OrdenProduccion
-            AND c.bCerrado = 0
+            --AND c.bCerrado = 0
             AND c.bAnulado = 0
             AND c.IdtdDocumentoForm = 127
         INNER JOIN dbo.docOrdenVenta g WITH (NOLOCK)
