69466e27f4369b3512f530b02b3b4ee0f9b2ad7c
rvfx04 <rvfx04@gmail.com>
2024-10-04 15:15:49 -0500

    Delete menu1.py

--- menu1.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def main():
-    st.title("Confección")
-    st.markdown("[Pedido](https://appgtpedido.streamlit.app)")
-    st.markdown("[OP](https://appgtop.streamlit.app)")
-
-if __name__ == '__main__':
-    main()
